141d4e066d6cf67086fcdd9eeccec8f38c40f718
Oleg Klimchuk <koi196621@gmail.com>
2021-05-20 18:04:17 +0300

    Add form history 3

--- src/main/java/gai/data/springcourse/controllers/KartaController.java
+++ src/main/java/gai/data/springcourse/controllers/KartaController.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.sql.SQLException;
 import java.util.List;
 
 @Controller
@@ -29,8 +27,6 @@ public class KartaController {
 
   @GetMapping("/searchAMT")
   public String searchAMT(@ModelAttribute("karta") KartaAMT karta, Model model) {
-//    final List<KartaAMT> kartaAMTList = kartaDAO.serch(karta);
-//    model.addAttribute("kartaList", kartaAMTList);
     return "karta/searchAMT";
   }
 
@@ -43,9 +39,9 @@ public class KartaController {
     return "karta/viewKarta";
   }
 
-    @GetMapping("/{kart_id}/history")
-    public String AmtHystory( @PathVariable("kart_id") String Kart_id, Model model) {
-        final List<KartaAMT> AMTHys = kartaDAO.AmtHistory(Kart_id);
+    @GetMapping( "/{kart_id}")
+    public String AmtHystory(@PathVariable("kart_id") String kart_id, Model model) {
+        List<KartaAMT> AMTHys = kartaDAO.AmtHistory(kart_id);
         model.addAttribute("Amthystory",AMTHys);
 
         return "karta/history";
